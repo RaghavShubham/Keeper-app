83a32b31a3ed9697e375ae517aac16abca173677
Shubham Raghav <shubham3raghav@gmail.com>
2020-10-15 00:22:26 +0530

    final commit

---
+++ src/Components/CreateArea.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function CreateArea(props){
+
+    const [noteContent, setNoteContent] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const {name , value} = event.target;
+        
+        setNoteContent( prevValue => {
+            return {
+                ...prevValue,
+                [name] : value
+            };
+        })
+    }
+
+    function newNote(event){
+        props.onAdd(noteContent);
+        setNoteContent({
+            title: "",
+            content: ""
+        }); 
+        event.preventDefault();
+    }
+
+    return(
+        <div>
+            <form>
+                <input
+                    onChange={handleChange} 
+                    name="title" 
+                    placeholder="Title" 
+                    value={noteContent.title}
+                />
+                <textarea 
+                    onChange={handleChange}
+                    name="content" 
+                    placeholder="Take a note..." 
+                    rows= "3" 
+                    value={noteContent.content}
+                />
+                <button onClick={newNote}>+</button>
+            </form>
+        </div>
+    );
+}
+export default CreateArea;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    var Year = new Date().getFullYear();
+   return(<footer>
+                <p> Copyright © {Year}</p>
+        </footer>);
+}
+
+export default Footer;
